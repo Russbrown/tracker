d30b1655e559a5b8abd095102cd1a2da09e8e98a
Russ Brown <poa02rb@yahoo.co.uk>
2013-11-28 17:01:34 +0000

    Ajax version updates every 5 secs

---
+++ grab.php
@@ -0,0 +1,23 @@
+<?php
+    $trackVal = $_REQUEST['val'];
+
+    $url = "https://ajax.googleapis.com/ajax/services/search/news?" .
+           "v=1.0&q=" . $trackVal . "&userip=82.71.236.173";
+
+    // sendRequest
+    // note how referer is set manually
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_REFERER, 'http://www.northerndiv.com');
+    $body = curl_exec($ch);
+    curl_close($ch);
+
+    // now, process the JSON string
+    echo($body);
+    // now have some fun with the results...
+
+    // var_dump($json);
+    // var_dump($json->responseData->results[0]);
+
+?>
--- js/main.js
+++ js/main.js
@@ -1,68 +1,59 @@
 
+
+
 $(function(){
 	$('.js-trackSubmit').bind('click', function(){
 		var trackVal = $('.js-trackVal').val();
-		$.ajax({
-			url: 'http://www.stoneacre.co.uk/search?&q=Ford&cx=001279084303569174969:lwcekl7iol4',
-			cache: false,
-			error: function(xhr, textstatus, errorThrown){
-				console.log('error');
-			},
-			success:function(data){
-				console.log(data);
-			}
-		});
-		return false
+		trackCallout(trackVal);
+		$(this).parents('.tracker-wrapper').addClass('active');
+		return false;
 	});
 
 	setInterval(function(){
-		// This code generates a "Raw Searcher" to handle search queries. The Raw
-		// Searcher requires you to handle and draw the search results manually.
-		google.load('search', '1');
-
-		var newsSearch;
-
-		function searchComplete() {
-
-		  // Check that we got results
-		  document.getElementById('content').innerHTML = '';
-		  if (newsSearch.results && newsSearch.results.length > 0) {
-		    for (var i = 0; i < newsSearch.results.length; i++) {
-
-		      // Create HTML elements for search results
-		      var p = document.createElement('p');
-		      var a = document.createElement('a');
-		      a.setAttribute("target", "_blank");
-		      
-		      var linky = newsSearch.results[i].url;
-		      var newlinky = decodeURIComponent(linky);
-		      a.href = newlinky;
-		      a.innerHTML = newsSearch.results[i].title;
-
-		      // Append search results to the HTML nodes
-		      p.appendChild(a);
-		      document.body.appendChild(p);
-		    }
-		  }
+		if ($('.tracker-wrapper').hasClass('active')) {
+			var trackVal = $('.js-trackVal').val();
+			trackCalloutReplace(trackVal);
 		}
-
-		function onLoad() {
-
-		  // Create a News Search instance.
-		  newsSearch = new google.search.NewsSearch();
-		  
-		  // Set searchComplete as the callback function when a search is
-		  // complete.  The newsSearch object will have results in it.
-		  newsSearch.setSearchCompleteCallback(this, searchComplete, null);
-
-		  // Specify search quer(ies)
-		  newsSearch.execute('Russ Brown');
-
-		  // Include the required Google branding
-		  // google.search.Search.getBranding('branding');
-		}
-
-		// Set a callback to call your code when the page loads
-		google.setOnLoadCallback(onLoad);
 	}, 5000);
-});
+});		
+
+function trackCallout(trackVal){
+	$.ajax({
+		url: 'grab.php',
+		cache: false,
+		data: 'val=' + trackVal,
+		dataType: 'json',
+		error: function(xhr, textstatus, errorThrown){
+			console.log('error');
+		},
+		success:function(data){
+			var i = 0;
+			$.each(data.responseData.results, function(index) {
+				var item = '<p><a href="' + data.responseData.results[index].unescapedUrl + '">' + data.responseData.results[index].title + '</a></p>';
+				$('#tracker1').append(item);
+			});
+		}
+	});
+
+}
+
+function trackCalloutReplace(trackVal){
+	$.ajax({
+		url: 'grab.php',
+		cache: false,
+		data: 'val=' + trackVal,
+		dataType: 'json',
+		error: function(xhr, textstatus, errorThrown){
+			console.log('error');
+		},
+		success:function(data){
+			var i = 0;
+			$('#tracker1').empty();
+			$.each(data.responseData.results, function(index) {
+				var item = '<p><a href="' + data.responseData.results[index].unescapedUrl + '">' + data.responseData.results[index].title + '</a></p>';
+				$('#tracker1').append(item);
+			});
+		}
+	});
+
+}
