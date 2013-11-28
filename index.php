69b391b501a388f23237d8d8d2f35dc3481c1b67
Russ Brown <poa02rb@yahoo.co.uk>
2013-11-28 12:58:53 +0000

    PHP Version
    Basic feed no updater

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+        </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php
+
+        $url = "https://ajax.googleapis.com/ajax/services/search/news?" .
+               "v=1.0&q=barack%20obama&userip=82.71.236.173";
+
+        // sendRequest
+        // note how referer is set manually
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, 'http://www.northerndiv.com');
+        $body = curl_exec($ch);
+        curl_close($ch);
+
+        // now, process the JSON string
+        $json = json_decode($body);
+        // now have some fun with the results...
+
+        // var_dump($json);
+        // var_dump($json->responseData->results[0]);
+        ?>
+
+        <!-- Add your site or application content here -->
+        <h1>Track your News</h1>
+        <?php 
+        foreach ($json->responseData->results as $result) {
+          echo('<a href="' . $result->clusterUrl . '">' . $result->title . '</a>');
+          echo('</br>');
+        }
+
+        ?>
+
+
+<!--         <div id="content" style="width:100%;">Loading...</div> -->
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
