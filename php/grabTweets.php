6942c8b4362ff3fa3290d556182d52ae94576c49
Russ Brown <poa02rb@yahoo.co.uk>
2013-12-05 16:05:39 +0000

    Gitignore updated and Login with twitter added

--- php/grabTweets.php
+++ php/grabTweets.php
@@ -3,15 +3,7 @@ require_once('TwitterAPIExchange.php');
 
 $trackVal = urlencode($_REQUEST['val']);
 
-
-/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-$settings = array(
-    'oauth_access_token' => "108106603-i2u0I7uBLGeoEteccCkTlVHVfhLHMhNqnWLw0QFs",
-    'oauth_access_token_secret' => "8uSAdtcI99uon1ZjafxTLlmDfJ05Sa6Ml8PFGs6dmgpXk",
-    'consumer_key' => "DEo6KAutLcMqbl3B9lqu6Q",
-    'consumer_secret' => "udFZhf6BaLdOwvFazFM5pa86ekiaMOX2WaBzDCCQNc"
-);
-
+require_once('settings.php');
 
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
 $getfield = '?q=' . $trackVal . '&result_type=popular&lang=en';
