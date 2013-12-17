c182b63768a14f0a0a953519d5bfb19a17790f1d
Russ Brown <poa02rb@yahoo.co.uk>
2013-12-17 14:16:53 +0000

    Login with twitter page styled

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Trakrific - Track the storys you're interested in</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
